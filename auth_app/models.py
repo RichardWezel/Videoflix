882c13f7e8552bb9c4d875eae7909790e6c3b776
Richard Wezel <richard.wezel@posteo.de>
2026-01-21 17:27:30 +0100

    feat:  changed standart user-model to email and added activation status

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    
+    account_activated = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"User {self.pk}: {self.email} (Active: {self.is_active})"
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users' 
