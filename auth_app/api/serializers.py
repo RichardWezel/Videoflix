2740e10d0f29ef3c0d562684136f9b4f4bde9ba5
Richard Wezel <richard.wezel@posteo.de>
2026-01-21 18:49:51 +0100

    feat: serializer for registration

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from auth_app.models import CustomUser
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    confirmed_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ( 'email', 'password', 'confirmed_password')
+        extra_kwargs = {'password': {'write_only': True}}   
+    
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['confirmed_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirmed_password')
+        user = CustomUser(
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
