8e48e0c4f17c378e1a0be4a4c86cbe93bd38af8e
Richard Wezel <richard.wezel@posteo.de>
2026-07-27 14:54:34 +0200

    feat: created Video model

--- video_app/models.py
+++ video_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    thumbnail_url = models.URLField(max_length=500)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title   
+    
+    class Meta:
+        verbose_name = "Video"
+        verbose_name_plural = "Videos"
+        odering = ['-created_at', 'title']
+    
