0545c052586f6c013f336b117568af500329a62c
Richard Wezel <richard.wezel@posteo.de>
2026-08-03 17:15:29 +0200

    feat:added django import export function

--- video_app/admin.py
+++ video_app/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 from .models import Video
 
 
+class VideoResource(resources.ModelResource):
+    class Meta:
+        model = Video
+
+
 @admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
+class VideoAdmin(ImportExportModelAdmin):
     list_display = ['id', 'title', 'category', 'created_at']
+    resource_class = VideoResource
